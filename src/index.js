28d5e6eabc01d4a67319798d9014be7ad7a0ef55
Juan Martin Restrepo <tinto922@gmail.com>
2022-08-15 11:05:49 -0400

    completed:

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function prepend(numSpaces, inputString) {
+    // your code here
+    for (var i = 0; i < numSpaces; i++) {
+        inputString = inputString + " ";
+    }
+    return inputString;
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-function prepend(numSpaces: number, inputString: string) {
+function prepend(numSpaces: number, inputString: string): string {
   // your code here
+  for(let i = 0; i < numSpaces; i++){
+    inputString = inputString + " "; 
+  }
+  return inputString; 
 }
+
+console.log(prepend(3, "bye"));
+// const answerElement  = document.getElementsByTagName("h2")[0]; 
+// answerElement.innerHTML = prepend(3, "testing"); 
